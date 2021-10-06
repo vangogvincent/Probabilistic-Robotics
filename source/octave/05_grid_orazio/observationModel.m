f8c36c03d0807b0eb43542386834b77a74cd7674
Barbara Bazzana <bazzana@diag.uniroma1.it>
2021-10-06 19:33:09 +0200

    code for exrcise 05_discrete_filtering

---
+++ source/octave/05_grid_orazio/observationModel.m
@@ -0,0 +1,35 @@
+function observation_probability = observationModel(map_, row_, col_, observations_)
+  observation_probability = 1;
+
+  #evaluate cell occupancy
+  cell_up_occupied    = 0;
+  cell_down_occupied  = 0;
+  cell_left_occupied  = 0;
+  cell_right_occupied = 0;
+  if (row_-1 > 0)
+	  cell_up_occupied    = map_(row_-1, col_);
+  endif
+  if (row_+1 <= rows(map_))
+    cell_down_occupied  = map_(row_+1, col_);
+  endif
+  if (col_-1 > 0)
+	  cell_left_occupied  = map_(row_, col_-1);
+  endif
+  if (col_+1 <= columns(map_))
+	  cell_right_occupied = map_(row_, col_+1);
+  endif
+
+  #update probability depending on observations
+  if (cell_up_occupied == observations_(1))
+	%TODO
+  endif
+  if (cell_down_occupied == observations_(2))
+	%TODO
+  endif
+  if (cell_left_occupied == observations_(3))
+	%TODO
+  endif
+  if (cell_right_occupied == observations_(4))
+	%TODO
+  endif
+endfunction
